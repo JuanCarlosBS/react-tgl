90231b274b618db5cec0c293cdf2995d224083eb
juancarlosbs <juanrov_@hotmail.com>
2021-05-17 15:03:13 -0300

    styled: Register Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import NewBet from './pages/NewBet';
 import RecentGames from './pages/RecentGames';
 import LogIn from './pages/LogIn';
 import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
   return (
     <Fragment>
-      <Register />
+      <ForgotPassword />
     </Fragment>
   );
 }
---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,22 @@
+import React,{ Fragment } from 'react';
+
+import Logo from '../../components/Logo'
+import { Container, TitleText, FormAuthentication, Form, Input, SubmitButton, LinkSingUpButton, LastInput } from './styles'
+
+const Register = () => {
+    return (
+        <Container>
+            <Logo />
+            <FormAuthentication>
+                <TitleText>Reset Password</TitleText>
+                <Form>
+                    <Input placeholder="Email" />
+                    <SubmitButton>Send link</SubmitButton>
+                </Form>
+                <LinkSingUpButton>Back</LinkSingUpButton>
+            </FormAuthentication>
+        </Container>
+    )
+}
+
+export default Register
