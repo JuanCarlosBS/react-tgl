f11ea1cd045deaece1af2e7ed0caead6b1c94891
juancarlosbs <juanrov_@hotmail.com>
2021-05-20 12:00:51 -0300

    feat: create cart store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux'
 
 import NewBet from './pages/NewBet';
 import RecentGames from './pages/RecentGames';
@@ -7,30 +8,34 @@ import LogIn from './pages/LogIn';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword'
 
+import store from './store';
+
 function App() {
   return (
-    <Fragment>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="log-in" />
-        </Route>
-        <Route path="/log-in">
-          <LogIn/>  
-        </Route>
-        <Route path="/forgot-password">
-          <ForgotPassword/>
-        </Route>
-        <Route path="/register">
-          <Register/>
-        </Route>
-        <Route path="/recent-games">
-          <RecentGames/>
-        </Route>
-        <Route path="/new-bet">
-          <NewBet/>
-        </Route>
-      </Switch>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="log-in" />
+          </Route>
+          <Route path="/log-in">
+            <LogIn/>  
+          </Route>
+          <Route path="/forgot-password">
+            <ForgotPassword/>
+          </Route>
+          <Route path="/register">
+            <Register/>
+          </Route>
+          <Route path="/recent-games">
+            <RecentGames/>
+          </Route>
+          <Route path="/new-bet">
+            <NewBet/>
+          </Route>
+        </Switch>
+      </Fragment>
+    </Provider>
   );
 }
 
